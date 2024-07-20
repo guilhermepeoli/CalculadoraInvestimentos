ff8927ace19f679acd934c904c30366134a1dbc7
guilhermepeoli <66880280+guilhermepeoli@users.noreply.github.com>
2024-07-20 12:29:36 -0300

    update

--- src/investmentGoals.js
+++ src/investmentGoals.js
@@ -9,7 +9,7 @@ export function generateReturnsArray(startingAmount = 0, timeHorizon = 0, timePe
     }
 
     const finalReturnRate = returnTimeFrame === 'monthly' ? 1 + (returnRate / 100) : convertToMonthlyReturnRate(1 + (returnRate / 100));
-    const finalTimeHorizon = timeHorizon === 'monthly' ? timeHorizon : timeHorizon * 12
+    const finalTimeHorizon = timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12
 
     const referenceInvestmentObject = {
         investedAmount: startingAmount,
@@ -18,7 +18,7 @@ export function generateReturnsArray(startingAmount = 0, timeHorizon = 0, timePe
         month: 0,
         totalAmount: startingAmount
     };
-    const returnsArray = [referenceInvestmentObject, {}];
+    const returnsArray = [referenceInvestmentObject];
     for (let timeReference = 1; timeReference <= finalTimeHorizon; timeReference++) {
         const totalAmount = returnsArray[timeReference - 1].totalAmount * finalReturnRate + monthlyContribution;
         const interestReturns = returnsArray[timeReference - 1].totalAmount * finalReturnRate;
